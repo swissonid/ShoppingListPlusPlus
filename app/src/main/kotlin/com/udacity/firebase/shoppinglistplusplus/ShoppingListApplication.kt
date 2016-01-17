6a12876fd769255a629b6846d216a8fc39204033
Patrice Mueller <mueller.patrice@gmail.com>
2016-01-17 10:34:14 +0100

    Added Kotlin to the Project

---
+++ app/src/main/kotlin/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.kt
@@ -0,0 +1,14 @@
+package com.udacity.firebase.shoppinglistplusplus
+
+import android.app.Application
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+class ShoppingListApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
